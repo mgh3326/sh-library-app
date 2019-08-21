fc643de4eccc61fee4a9e9db97752ed2f5a40fa0
kwanghyunMoon <mgh3326@naver.com>
2019-08-21 19:12:27 +0900

    create activity

---
+++ app/src/main/java/me/khmoon/sh_library_app/LoginActivity.java
@@ -0,0 +1,14 @@
+package me.khmoon.sh_library_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
